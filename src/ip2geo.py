f5fca181b36be88943c9414779dd7518a4473ab4
0x1p2 <ry4.ball@gmail.com>
2016-02-17 16:11:05 -0600

    Started placing into single class

--- src/ip2geo.py
+++ src/ip2geo.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 import json
+import argparse
 from configparser import ConfigParser
 
 def conf_read():
@@ -15,62 +16,81 @@ def conf_read():
     else:
         return 0
 
+class Lookup():
 
-cc = {} # Holds a KEY:VALUE for COUNTRY_CODE:AMOUNT. Amount will be the number of discovered attempts.
+    def __init__(self, args):
+        self.host = args.url
+        self.verbose = args.verbose
 
-# Country codes obtained from: http://www.countrycallingcodes.com/iso-country-codes/
-eu_codes = [ 
-        "AL", "AD", "AT", "BY", "BE", "BA", "BG", "HR", "CY",
-        "CZ", "DK", "EE", "FO", "FI", "FR", "DE", "GI", "GR", 
-        "HU", "IS", "IE", "IT", "LV", "LI", "IT", "LU", "MK",
-        "MT", "MD", "MC", "NL", "NO", "PL", "PT", "RO", "RU",
-        "SM", "RS", "SK", "SI", "ES", "SE", "CH", "UA", "GB",
-        "VA", "RS", "IM", "RS", "ME" ]
-na_codes = [
-        "AI", "AG", "AW", "BS", "BB", "BZ", "BM", "VG", "CA",
-        "KY", "CR", "CU", "CW", "DM", "DO", "SV", "GL", "GD", 
-        "GP", "GT", "HT", "HN", "JM", "MQ", "MX", "PM", "MS",
-        "CW", "KN", "NI", "PA", "PR", "LC", "VC", "TT", "TC",
-        "VI", "US", "SX", "BQ", "SA", "SE" ]
-sa_codes = [
-        "AR", "BO", "BR", "CL", "CO", "EC", "FK", "GF", "GY",
-        "PY", "PE", "SR", "UY", "VE" ]
-af_codes = [
-        "DZ", "AO", "SH", "BJ", "BW", "BF", "BI", "CM", "CV",
-        "CF", "TD", "KM", "CG", "DJ", "EG", "GQ", "ER", "ET",
-        "GA", "GM", "GH", "GW", "GN", "CI", "KE", "LS", "LR",
-        "LY", "MG", "MW", "ML", "MR", "MU", "YT", "MA", "MZ",
-        "NA", "NE", "NG", "ST", "RE", "RW", "ST", "SN", "SC",
-        "SL", "SO", "ZA", "SH", "SD", "SZ", "TZ", "TG", "TN",
-        "UG", "CD", "ZM", "TZ", "ZW", "SS", "CD" ]
-as_codes = [
-        "AF", "AM", "AZ", "BH", "BD", "BT", "BN", "KH", "CN",
-        "CX", "CC", "IO", "GE", "HK", "IN", "ID", "IR", "IQ",
-        "IL", "JP", "JO", "KZ", "KP", "KR", "KW", "KG", "LA",
-        "LB", "MO", "MY", "MV", "MN", "MM", "NP", "OM", "PK",
-        "PH", "QA", "SA", "SG", "LK", "SY", "TW", "TJ", "TH",
-        "TR", "TM", "AE", "UZ", "VN", "YE", "PS" ]
-au_codes = [
-        "AS", "AU", "NZ", "CK", "FJ", "PF", "GU", "KI", "MP",
-        "MH", "FM", "UM", "NR", "NC", "NZ", "NU", "NF", "PW",
-        "PG", "MP", "SB", "TK", "TO", "TV", "VU", "UM", "WF",
-        "WS", "TL" ]
-aq_codes = [ "AQ" ]
+        self.cc = {} # Holds a KEY:VALUE for COUNTRY_CODE:AMOUNT. Amount will be the number of discovered attempts.
+        # Country codes obtained from: http://www.countrycallingcodes.com/iso-country-codes/
+        self.eu_codes = [ 
+                "AL", "AD", "AT", "BY", "BE", "BA", "BG", "HR", "CY",
+                "CZ", "DK", "EE", "FO", "FI", "FR", "DE", "GI", "GR", 
+                "HU", "IS", "IE", "IT", "LV", "LI", "IT", "LU", "MK",
+                "MT", "MD", "MC", "NL", "NO", "PL", "PT", "RO", "RU",
+                "SM", "RS", "SK", "SI", "ES", "SE", "CH", "UA", "GB",
+                "VA", "RS", "IM", "RS", "ME" ]
+        self.na_codes = [
+                "AI", "AG", "AW", "BS", "BB", "BZ", "BM", "VG", "CA",
+                "KY", "CR", "CU", "CW", "DM", "DO", "SV", "GL", "GD", 
+                "GP", "GT", "HT", "HN", "JM", "MQ", "MX", "PM", "MS",
+                "CW", "KN", "NI", "PA", "PR", "LC", "VC", "TT", "TC",
+                "VI", "US", "SX", "BQ", "SA", "SE" ]
+        self.sa_codes = [
+                "AR", "BO", "BR", "CL", "CO", "EC", "FK", "GF", "GY",
+                "PY", "PE", "SR", "UY", "VE" ]
+        self.af_codes = [
+                "DZ", "AO", "SH", "BJ", "BW", "BF", "BI", "CM", "CV",
+                "CF", "TD", "KM", "CG", "DJ", "EG", "GQ", "ER", "ET",
+                "GA", "GM", "GH", "GW", "GN", "CI", "KE", "LS", "LR",
+                "LY", "MG", "MW", "ML", "MR", "MU", "YT", "MA", "MZ",
+                "NA", "NE", "NG", "ST", "RE", "RW", "ST", "SN", "SC",
+                "SL", "SO", "ZA", "SH", "SD", "SZ", "TZ", "TG", "TN",
+                "UG", "CD", "ZM", "TZ", "ZW", "SS", "CD" ]
+        self.as_codes = [
+                "AF", "AM", "AZ", "BH", "BD", "BT", "BN", "KH", "CN",
+                "CX", "CC", "IO", "GE", "HK", "IN", "ID", "IR", "IQ",
+                "IL", "JP", "JO", "KZ", "KP", "KR", "KW", "KG", "LA",
+                "LB", "MO", "MY", "MV", "MN", "MM", "NP", "OM", "PK",
+                "PH", "QA", "SA", "SG", "LK", "SY", "TW", "TJ", "TH",
+                "TR", "TM", "AE", "UZ", "VN", "YE", "PS" ]
+        self.au_codes = [
+                "AS", "AU", "NZ", "CK", "FJ", "PF", "GU", "KI", "MP",
+                "MH", "FM", "UM", "NR", "NC", "NZ", "NU", "NF", "PW",
+                "PG", "MP", "SB", "TK", "TO", "TV", "VU", "UM", "WF",
+                "WS", "TL" ]
+        self.aq_codes = [ "AQ" ]
 
-eu_cntr = 0
-na_cntr = 0
-sa_cntr = 0
-af_cntr = 0
-as_cntr = 0
-au_cntr = 0
-aq_cntr = 0
-unk_cntr = 0
+        self.eu = 0
+        self.na = 0
+        self.sa = 0
+        self.af = 0
+        self.as = 0
+        self.au = 0
+        self.aq = 0
+        self.unk  = 0
 
-if os.path.exists("config.ini"):
-    config = conf_read()
-    host = config['URL']['host_db']
-else:
-    host = "http://freegeoip.net/"
+    def set_host(self):
+        if not self.url:
+            if not os.path.exists("config.ini"):
+                self.host = "http://freegeoip.net/"
+            else:
+                config = conf_read()
+                self.host = config['URL']['host_db']
+        else:
+            self.host 
+
+
+options = argparse.ArgumentParser(description="A tool that takes in the input of an IP and returns the GeoLocation data associated with it.")
+group = options.add_mutually_exclusive_group()
+group.add_argument('-q', '--quiet', help="no output except region amount.",
+        action='store_false', dest="verbose")
+group.add_argument('-v', '--verbose', help="set verbose output [default]",
+        action='store_true', dest="verbose")
+options.add_argument('-u', '--url', help="alternate url to use as a db",
+        default=False)
+args = options.parse_args()
 
 with open('attackers.txt', 'r') as fd:
     file_data = fd.readlines()
