dc493babc2d186183546560134ed84db5f9f891b
0x1p2 <ry4.ball@gmail.com>
2016-02-23 14:51:47 -0600

    Placed into a class, Will allow for better manipulation.

--- src/ip2geo.py
+++ src/ip2geo.py
@@ -1,28 +1,18 @@
 import os
+import sys
 import urllib.request
 import json
 import argparse
-from configparser import ConfigParser
+import configparser
 
-def conf_read():
-    ''' Loads the configuration file if one is discovered. This is implemented 
-    because I use a local database instead of the freegeoip.net so I can make 
-    frequent queries for testing. '''
-    if os.path.exists('config.ini'):
-        print("Loading configuration file...")
-        config = ConfigParser()
-        config.read('config.ini')
-        return config
-    else:
-        return 0
-
-class Lookup():
-
+class Lookup:
     def __init__(self, args):
-        self.host = args.url
+        self.url = args.url
         self.verbose = args.verbose
+        self.FILE = args.FILE
 
         self.cc = {} # Holds a KEY:VALUE for COUNTRY_CODE:AMOUNT. Amount will be the number of discovered attempts.
+        self.reg = { "EU":0, "NA":0, "SA":0, "AF":0, "ASIA":0, "AUS":0, "AQ":0, "UNK":0 }
         # Country codes obtained from: http://www.countrycallingcodes.com/iso-country-codes/
         self.eu_codes = [ 
                 "AL", "AD", "AT", "BY", "BE", "BA", "BG", "HR", "CY",
@@ -48,38 +38,105 @@ class Lookup():
                 "NA", "NE", "NG", "ST", "RE", "RW", "ST", "SN", "SC",
                 "SL", "SO", "ZA", "SH", "SD", "SZ", "TZ", "TG", "TN",
                 "UG", "CD", "ZM", "TZ", "ZW", "SS", "CD" ]
-        self.as_codes = [
+        self.asia_codes = [
                 "AF", "AM", "AZ", "BH", "BD", "BT", "BN", "KH", "CN",
                 "CX", "CC", "IO", "GE", "HK", "IN", "ID", "IR", "IQ",
                 "IL", "JP", "JO", "KZ", "KP", "KR", "KW", "KG", "LA",
                 "LB", "MO", "MY", "MV", "MN", "MM", "NP", "OM", "PK",
                 "PH", "QA", "SA", "SG", "LK", "SY", "TW", "TJ", "TH",
                 "TR", "TM", "AE", "UZ", "VN", "YE", "PS" ]
-        self.au_codes = [
+        self.aus_codes = [
                 "AS", "AU", "NZ", "CK", "FJ", "PF", "GU", "KI", "MP",
                 "MH", "FM", "UM", "NR", "NC", "NZ", "NU", "NF", "PW",
                 "PG", "MP", "SB", "TK", "TO", "TV", "VU", "UM", "WF",
                 "WS", "TL" ]
         self.aq_codes = [ "AQ" ]
 
-        self.eu = 0
-        self.na = 0
-        self.sa = 0
-        self.af = 0
-        self.as = 0
-        self.au = 0
-        self.aq = 0
-        self.unk  = 0
+
+    def conf_read(self):
+        ''' Loads the configuration file if one is discovered. This is implemented 
+        because I use a local database instead of the freegeoip.net so I can make 
+        frequent queries for testing. '''
+        if os.path.exists('config.ini'):
+            print("Loading configuration file...")
+            config = configparser.ConfigParser()
+            config.read('config.ini')
+            return config
+        else:
+            return 0
 
     def set_host(self):
         if not self.url:
             if not os.path.exists("config.ini"):
                 self.host = "http://freegeoip.net/"
             else:
-                config = conf_read()
+                config = self.conf_read()
                 self.host = config['URL']['host_db']
         else:
-            self.host 
+            self.host = self.url    # Add a check to see if it is availible, if not default to localhost.
+        print("Using: [ %s ]" % self.host)
+
+
+    def set_list(self):
+        if not os.path.exists(self.FILE):
+            print("No file %s found." % self.FILE)
+            sys.exit()            
+        else:
+            with open(self.FILE) as fd:
+                self.IPs = fd.readlines()
+
+
+    def query(self):
+        count = 0
+        print("Starting query...")
+        for IP in self.IPs:
+            url = self.host + "json/" + IP
+            count += 1
+            sent = urllib.request.urlopen(url).read()
+            data = sent.decode()
+            info = json.loads(data)
+            self.parse(info)
+
+
+    def parse(self, info):
+        code = info['country_code']
+        if code == "":
+            code = "UNK"
+        if code in self.cc:
+            self.cc[code] += 1
+        else:
+            self.cc[code] = 1
+
+        if code in self.eu_codes:
+            self.reg['EU'] += 1
+        elif code in self.na_codes:
+            self.reg['NA'] += 1
+        elif code in self.sa_codes:
+            self.reg['SA'] += 1
+        elif code in self.af_codes:
+            self.reg['AF'] += 1
+        elif code in self.asia_codes:
+            self.reg['ASIA'] += 1
+        elif code in self.aus_codes:
+            self.reg['AUS'] += 1
+        elif code in self.aq_codes:
+            self.reg['AQ'] += 1
+        else:
+            self.reg['UNK'] += 1
+
+
+    def printf(self):
+        for key, value in self.reg.items():     # Could be changed to self.cc
+            print("{:>5} | {:<5}".format(key, value))
+
+
+    def start(self):
+        self.set_host()
+        self.set_list()
+        self.query()
+        self.printf()
+
+
 
 
 options = argparse.ArgumentParser(description="A tool that takes in the input of an IP and returns the GeoLocation data associated with it.")
@@ -90,52 +147,9 @@ group.add_argument('-v', '--verbose', help="set verbose output [default]",
         action='store_true', dest="verbose")
 options.add_argument('-u', '--url', help="alternate url to use as a db",
         default=False)
+options.add_argument('FILE', help="file to parse.")
 args = options.parse_args()
 
-with open('attackers.txt', 'r') as fd:
-    file_data = fd.readlines()
+my_search = Lookup(args)
+my_search.start()
 
-for IP in file_data:
-    url = host + "json/" + IP
-    response = urllib.request.urlopen(url).read()
-    data = response.decode()
-    response  = json.loads(data)
-    code = response['country_code']
-    if code == "":
-        code = "UNK"
-
-    if code in cc:
-        cc[code] += 1
-    else:
-        cc[code] = 1
-
-    #print("{:<15} {:<3} {:<6}".format(response['ip'], response['country_code'], response['region_code']))
-
-#for KEY, PAIR in cc.items():
-#    print("{:<3} {:<4}".format(KEY, PAIR))
-
-    if code in eu_codes:
-        eu_cntr += 1
-    elif code in na_codes:
-        na_cntr += 1
-    elif code in sa_codes:
-        na_cntr += 1
-    elif code in af_codes:
-        af_cntr += 1
-    elif code in as_codes:
-        as_cntr += 1
-    elif code in au_codes:
-        au_cntr += 1
-    elif code in aq_codes:
-        aq_cntr += 1
-    else:
-        unk_cntr += 1
-
-print("EU: %d" % eu_cntr)
-print("NA: %d" % na_cntr)
-print("SA: %d" % sa_cntr)
-print("AF: %d" % af_cntr)
-print("AS: %d" % as_cntr)
-print("AU: %d" % au_cntr)
-print("AQ: %d" % aq_cntr)
-print("UNKNOWN: %d" % unk_cntr)
